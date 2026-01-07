8ac115038c4fc79e80b46a1d490dfc9054fd2e4e
Momoko Nagashima <nagashima.m.dev@gmail.com>
2026-01-07 13:09:33 +0900

    Breezeによる認証機能を導入

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,26 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
-Route::resource('posts', PostController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// ログイン後のホーム（Breeze標準）
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    // Breeze標準
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // posts をログイン必須に
+    Route::resource('posts', PostController::class);
+});
+
+require __DIR__ . '/auth.php';
