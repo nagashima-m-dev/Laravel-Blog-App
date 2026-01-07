767ca633ceb100604d2be1ca8f02378c35ae37f1
Momoko Nagashima <nagashima.m.dev@gmail.com>
2026-01-07 11:54:38 +0900

    投稿の削除機能を追加

--- backend/resources/views/posts/show.blade.php
+++ backend/resources/views/posts/show.blade.php
@@ -3,4 +3,12 @@
 <p>{{ $post->body }}</p>
 
 <p><a href="{{ route('posts.edit', $post) }}">編集</a></p>
+
+<form method="POST" action="{{ route('posts.destroy', $post) }}" onsubmit="return confirm('本当に削除しますか？');">
+    @csrf
+    @method('DELETE')
+
+    <button type="submit">削除</button>
+</form>
+
 <p><a href="{{ route('posts.index') }}">一覧に戻る</a></p>
