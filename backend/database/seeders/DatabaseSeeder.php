6379d323cc40ce7cdac886848991e1e60f1d87a5
Momoko Nagashima <nagashima.m.dev@gmail.com>
2026-01-14 18:36:27 +0900

    README更新

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,21 +5,18 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'testuser',
+            'email' => 'test@test.com',
+            'password' => Hash::make('testpass'),
         ]);
     }
 }
